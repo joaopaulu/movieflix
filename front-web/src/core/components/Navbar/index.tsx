08c6e45451d40abb8061406f61f7cc2644e60e93
João Paulo Lima <jptick@gmail.com>
2021-03-11 16:44:51 -0300

    Update Navbar Component

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,13 +1,44 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { getAccessTokenDecoded, logout } from 'core/utils/auth';
+import { useEffect, useState } from 'react';
 
 import './styles.scss';
 
 const Navbar = () => {
+  const [currentUser, setCurrentUser] = useState('');
+  const location = useLocation();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    const currentUserData = getAccessTokenDecoded();
+    setCurrentUser(currentUserData.user_name);
+  }, [location]);
+
   return (
-    <nav className="bg-primary main-nav">
-      <Link to="/" className="nav-logo-text">
-        <h4>MovieFlix</h4>
-      </Link>
+    <nav className="bg-primary main-nav row">
+      <div className="col-11">
+        <Link to="/" className="nav-logo-text">
+          <h4>MovieFlix</h4>
+        </Link>
+      </div>
+      <div className="col-1">
+        <div className="user-info-dnone text-right">
+          {currentUser && (
+            <>
+              <button
+                type="button"
+                className="btn-out btn-outline-secondary btn"
+                onClick={handleLogout}
+              >
+                Sair
+              </button>
+            </>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
