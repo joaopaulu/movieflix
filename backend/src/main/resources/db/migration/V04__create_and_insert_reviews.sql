e0d89e1504b4397b4cffa5bcbcbd15aef87896df
João Paulo <jptick@gmail.com>
2021-03-26 15:45:02 -0300

    Updade Migrations Reviews

--- backend/src/main/resources/db/migration/V04__create_and_insert_reviews.sql
+++ backend/src/main/resources/db/migration/V04__create_and_insert_reviews.sql
@@ -10,7 +10,32 @@ alter table reviews add constraint FK5r4sbavb4nkd9xpl0f095qs2a foreign key (movi
 alter table reviews add constraint FKgbof0jclmaf8wn2alsoexxq3u foreign key (user_id) references users;
 
 INSERT INTO reviews (text, movie_id, user_id) VALUES
-('Teste', 1, 1);
+('Good, Good, Good', 1, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 2, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 7, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 8, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 9, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 10, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 11, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 12, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 13, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 14, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 15, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 16, 2);
+INSERT INTO reviews (text, movie_id, user_id) VALUES
+('Good, Good, Good', 17, 2);
+
 
 
 
